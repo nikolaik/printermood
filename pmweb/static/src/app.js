e0392271657e9f09f96791f135ba5ccdc117d83e
Nikolai R Kristiansen <nikolaik@gmail.com>
2016-03-20 18:01:43 +0100

    feat(web): Face list style updates
    - Also switch to sass preprocessor

--- pmweb/static/src/app.js
+++ pmweb/static/src/app.js
@@ -1,6 +1,6 @@
 require('material-design-lite/material.min.css');
 require('material-design-lite/material.min.js');
-require('./css/style.css');
+require('./css/style.scss');
 
 // Example module usage
 //var yell = require('./js/alert.js');
